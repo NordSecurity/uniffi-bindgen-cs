57d9c370c62cd0ca119e2e47b2ef67dbc6af64b8
Kristupas Antanavicius <kristupas.antanavicius@nordsec.com>
2023-03-23 14:13:58 +0200

    Fix recompilation upon every build
    Signed-off-by: Kristupas Antanavicius <kristupas.antanavicius@nordsec.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-cargo build --bin uniffi-bindgen-cs
-cargo build --lib --features uniffi_fixtures
+cargo build --bin uniffi-bindgen-cs --lib --features uniffi_fixtures
