0ced6f0195f3dfbf345f87eba3d45ce1ae856e17
Kristupas Antanavicius <kristupas.antanavicius@nordsec.com>
2023-10-23 15:52:03 +0300

    Implement `Display` trait

---
+++ dotnet-tests/UniffiCS.binding_tests/TestTraitMethod.cs
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+using uniffi.trait_methods;
+
+public class TestTraitMethods {
+    [Fact]
+    public void TraitMethodsWork() {
+        using (var methods = new TraitMethods("yo")) {
+            Assert.Equal("TraitMethods(yo)", methods.ToString());
+        }
+    }
+
+    [Fact]
+    public void TraitMethodsProcMacro() {
+        using (var methods = new ProcTraitMethods("yo")) {
+            Assert.Equal("ProcTraitMethods(yo)", methods.ToString());
+        }
+    }
+}
+
