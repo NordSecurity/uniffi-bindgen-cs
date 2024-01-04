3629b4f7e2731389524b9895061b5f05115bb461
Samuel Meenzen <samuel@meenzen.net>
2024-01-04 19:32:06 +0100

    refactor: combine test projects
    Signed-off-by: Samuel Meenzen <samuel@meenzen.net>

--- dotnet-tests/UniffiCS.BindingTests/TestBigEndianStream.cs
+++ dotnet-tests/UniffiCS.BindingTests/TestBigEndianStream.cs
@@ -2,14 +2,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-namespace UniffiCS.Tests;
-
 using System;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
+using uniffi.arithmetic;
+
+namespace UniffiCS.BindingTests;
 
 public class TestBigEndianStream
 {
