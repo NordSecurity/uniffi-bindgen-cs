ce23ac2f7d919b51b6817b534c559c7f416b6d89
Samuel Meenzen <samuel@meenzen.net>
2024-01-04 19:44:41 +0100

    feat: run all tests in the UniffiCS solution instead of specifying a single project
    Signed-off-by: Samuel Meenzen <samuel@meenzen.net>

--- test_bindings.sh
+++ test_bindings.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 
 SCRIPT_DIR="${SCRIPT_DIR:-$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )}"
 
-CSPROJ_DIR="dotnet-tests/UniffiCS.BindingTests"
+SOLUTION_DIR="dotnet-tests"
 
 export LD_LIBRARY_PATH="$SCRIPT_DIR/target/debug/:${LD_LIBRARY_PATH:-}"
-cd $CSPROJ_DIR
+cd $SOLUTION_DIR
 dotnet test -l "console;verbosity=normal"
