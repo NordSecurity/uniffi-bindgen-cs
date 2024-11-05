6dee66238364aed2e72f1e67da8c4964394557bf
Chloe McKnight <cmcknight@bluebeam.com>
2024-11-05 11:11:43 -0600

    implement Eq and Hash trait methods
    Signed-off-by: Chloe McKnight <cmcknight@bluebeam.com>

--- bindgen/templates/ObjectTemplate.cs
+++ bindgen/templates/ObjectTemplate.cs
@@ -7,7 +7,15 @@
 {%- if self.include_once_check("ObjectRuntime.cs") %}{% include "ObjectRuntime.cs" %}{% endif %}
 
 {%- call cs::docstring(obj, 0) %}
-{{ config.access_modifier() }} interface I{{ type_name }} {
+{{ config.access_modifier() }} interface I{{ type_name }}
+    {%- for tm in obj.uniffi_traits() -%}
+    {%- if loop.first -%} : {% endif -%}
+    {%- match tm -%}
+    {%- when UniffiTrait::Eq { eq, ne } -%}
+    IEquatable<{{type_name}}>
+    {%- else -%}
+    {%- endmatch -%}
+    {%- endfor %} {
     {% for meth in obj.methods() -%}
     {%- call cs::docstring(meth, 4) %}
     {%- call cs::method_throws_annotation(meth.throws_type()) %}
@@ -62,6 +70,31 @@
     {%- when UniffiTrait::Display { fmt } %}
     public override string ToString() {
         return {{ Type::String.borrow()|lift_fn }}({%- call cs::to_ffi_call_with_prefix("this.GetHandle()", fmt) %});
+    }
+    {%- when UniffiTrait::Eq { eq, ne } %}
+    public bool Equals({{type_name}}? other)
+    {
+        if (other is null) return false;
+        return {{ Type::Boolean.borrow()|lift_fn }}({%- call cs::to_ffi_call_with_prefix("this.GetHandle()", eq) %});
+    }
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || !(obj is {{type_name}})) return false;
+        return Equals(obj as {{type_name}});
+    }
+    public static bool operator == ({{type_name}}? a, {{type_name}}? b)
+    {
+        if (a is null || b is null) return Object.Equals(a, b);
+        return a.Equals(b);
+    }
+    public static bool operator != ({{type_name}}? a, {{type_name}}? b)
+    {
+        if (a is null || b is null) return !Object.Equals(a, b);
+        return !(a.Equals(b));
+    }
+    {%- when UniffiTrait::Hash  { hash }  %}
+    public override int GetHashCode() { 
+        return (int){{ Type::UInt64.borrow()|lift_fn }}({%- call cs::to_ffi_call_with_prefix("this.GetHandle()", hash)  %});
     }
     {%- else %}
     {%- endmatch %}
--- dotnet-tests/UniffiCS.BindingTests/TestTraitMethod.cs
+++ dotnet-tests/UniffiCS.BindingTests/TestTraitMethod.cs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+using System;
+using System.Collections.Generic;
 using uniffi.trait_methods;
 
 namespace UniffiCS.BindingTests;
@@ -9,7 +11,7 @@ namespace UniffiCS.BindingTests;
 public class TestTraitMethods
 {
     [Fact]
-    public void TraitMethodsWork()
+    public void TestDisplay()
     {
         using (var methods = new TraitMethods("yo"))
         {
@@ -18,11 +20,118 @@ public class TestTraitMethods
     }
 
     [Fact]
-    public void TraitMethodsProcMacro()
+    public void TestEq()
+    {
+        using (var methods = new TraitMethods("yo"))
+        {
+            Assert.Equal(methods, new TraitMethods("yo"));
+            Assert.True(methods == new TraitMethods("yo"));
+
+            Assert.Equal(methods, methods);
+
+            Assert.NotEqual(methods, new TraitMethods("yoyo"));
+            Assert.True(methods != new TraitMethods("yoyo"));
+        }
+
+        Assert.False(new TraitMethods("yo") == null);
+    }
+
+    [Fact]
+    public void TestEqNulls()
+    {
+        TraitMethods? t1 = null;
+        TraitMethods? t2 = null;
+        Assert.True(Object.Equals(t1, t2));
+
+        t1 = new TraitMethods("yo");
+        Assert.False(Object.Equals(t1, t2));
+        
+        Assert.False(new TraitMethods("yo") == null);
+        Assert.True(new TraitMethods("yo") != null);
+    }
+
+    [Fact]
+    public void TestEqContains() 
+    {
+        var tm = new TraitMethods("yo");
+        var list = new List<TraitMethods>();
+        list.Add(tm);
+
+        Assert.Contains(tm, list);
+        Assert.Contains(new TraitMethods("yo"), list);
+        Assert.DoesNotContain(null, list);
+        Assert.DoesNotContain(new TraitMethods("yoyo"), list);
+    }
+
+   [Fact]
+   public void TestHash()
+   {
+       using (var methods = new TraitMethods("yo"))
+       {
+           Assert.Equal(methods.GetHashCode(), new TraitMethods("yo").GetHashCode());
+           Assert.NotEqual(methods.GetHashCode(), new TraitMethods("yoyo").GetHashCode());
+       }
+   }
+}
+
+public class TestProcMacroTraitMethods
+{
+    [Fact]
+    public void TestDisplay()
     {
         using (var methods = new ProcTraitMethods("yo"))
         {
             Assert.Equal("ProcTraitMethods(yo)", methods.ToString());
+        }
+    }
+
+   [Fact]
+    public void TestEq()
+    {
+        using (var methods = new ProcTraitMethods("yo"))
+        {
+            Assert.Equal(methods, new ProcTraitMethods("yo"));
+            Assert.True(methods == new ProcTraitMethods("yo"));
+
+            Assert.Equal(methods, methods);
+
+            Assert.NotEqual(methods, new ProcTraitMethods("yoyo"));
+            Assert.True(methods != new ProcTraitMethods("yoyo"));
+        }
+
+        Assert.False(new ProcTraitMethods("yo") == null);
+    }
+
+    [Fact]
+    public void TestEqNulls()
+    {
+        ProcTraitMethods? t1 = null;
+        ProcTraitMethods? t2 = null;
+        Assert.True(Object.Equals(t1, t2));
+        
+        Assert.False(new ProcTraitMethods("yo") == null);
+    }
+
+    [Fact]
+    public void TestEqContains() 
+    {
+        var tm = new ProcTraitMethods("yo");
+        var list = new List<ProcTraitMethods>();
+        list.Add(tm);
+
+        Assert.Contains(tm, list);
+        Assert.Contains(new ProcTraitMethods("yo"), list);
+        Assert.DoesNotContain(null, list);
+        Assert.DoesNotContain(new ProcTraitMethods("yoyo"), list);
+    }
+
+    [Fact]
+    public void TestHash()
+    {
+        using (var methods = new ProcTraitMethods("yo"))
+        {
+            Assert.Equal(methods.GetHashCode(), new ProcTraitMethods("yo").GetHashCode());
+            Assert.NotEqual(methods.GetHashCode(), new ProcTraitMethods("yoyo").GetHashCode());
         }
     }
 }
