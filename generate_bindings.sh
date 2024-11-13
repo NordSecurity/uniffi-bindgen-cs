4c621e1a5b5362b00c5d6258649cc3a18db5de67
Christian Belloni <christianbelloni@hotmail.it>
2024-11-13 13:00:37 +0100

    Add support for running tests on OSX (#96)

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -6,4 +6,12 @@ GEN_DIR="dotnet-tests/UniffiCS/gen"
 rm -rf "$GEN_DIR"
 mkdir -p "$GEN_DIR"
 
-target/debug/uniffi-bindgen-cs target/debug/libuniffi_fixtures.so --library --out-dir="$GEN_DIR" --no-format
+libname=""
+
+if [[ $(uname) == "Darwin" ]]; then
+  libname="libuniffi_fixtures.dylib"
+else
+  libname="libuniffi_fixtures.so"
+fi
+
+target/debug/uniffi-bindgen-cs "target/debug/$libname" --library --out-dir="$GEN_DIR" --no-format
