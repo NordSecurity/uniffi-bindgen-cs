222cd8a35b25d1a10c16a6b1d9fac90b9739b265
Ciprian Khlud <ciprian.khlud@nordsec.com>
2025-07-31 16:00:07 +0300

    Simplified Dispose call
    Signed-off-by: Ciprian Khlud <ciprian.khlud@nordsec.com>
    Signed-off-by: Mathews Bryan <37884221+jmbryan4@users.noreply.github.com>

--- bindgen/templates/Helpers.cs
+++ bindgen/templates/Helpers.cs
@@ -140,29 +140,50 @@ static class FFIObjectUtil {
     // Dispose is implemented by recursive type inspection at runtime. This is because
     // generating correct Dispose calls for recursive complex types, e.g. List<List<int>>
     // is quite cumbersome.
-    private static void Dispose(dynamic? obj) {
-        if (obj == null) {
-            return;
-        }
+   private static void Dispose(Object? obj) {
+         if (obj == null) {
+             return;
+         }
 
-        if (obj is IDisposable disposable) {
-            disposable.Dispose();
-            return;
-        }
+         if (obj is IDisposable disposable) {
+             disposable.Dispose();
+             return;
+         }
 
-        var type = obj.GetType();
-        if (type != null) {
-            if (type.IsGenericType) {
-                if (type.GetGenericTypeDefinition().IsAssignableFrom(typeof(List<>))) {
-                    foreach (var value in obj) {
-                        Dispose(value);
-                    }
-                } else if (type.GetGenericTypeDefinition().IsAssignableFrom(typeof(Dictionary<,>))) {
-                    foreach (var value in obj.Values) {
-                        Dispose(value);
-                    }
-                }
-            }
-        }
-    }
+         var objType = obj.GetType();
+         var typeCode = Type.GetTypeCode(objType);
+         if (typeCode != TypeCode.Object) {
+             return;
+         }
+
+         var genericArguments = objType.GetGenericArguments();
+         if (genericArguments.Length == 0) {
+             return;
+         }
+
+         if (obj is System.Collections.IDictionary objDictionary) {
+            //This extra code tests to not call "Dispose" for a Dictionary<something, double>()
+            //for all keys as "double" and alike doesn't support interface "IDisposable"
+             var valuesType = objType.GetGenericArguments()[1];
+             var elementValuesTypeCode = Type.GetTypeCode(valuesType);
+             if (elementValuesTypeCode != TypeCode.Object) {
+                 return;
+             }
+             foreach (var value in objDictionary.Values) {
+                 Dispose(value);
+             }
+         }
+         else if (obj is System.Collections.IEnumerable listValues) {
+            //This extra code tests to not call "Dispose" for a List<int>()
+            //for all keys as "int" and alike doesn't support interface "IDisposable"
+             var valuesType = objType.GetGenericArguments()[0];
+             var elementValuesTypeCode = Type.GetTypeCode(valuesType);
+             if (elementValuesTypeCode != TypeCode.Object) {
+                 return;
+             }
+             foreach (var value in listValues) {
+                 Dispose(value);
+             }
+         }
+     }
 }
