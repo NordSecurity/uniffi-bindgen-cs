5a49f9a90c3de0bce424af019e2f769ebb2f53a7
Mathews Bryan <37884221+jmbryan4@users.noreply.github.com>
2026-02-20 15:06:54 -0500

    feat: use LibraryImport instead of DllImport for .NET 8+ targets
    Generate [LibraryImport] with [UnmanagedCallConv] and partial methods
    for NET8_0_OR_GREATER, falling back to [DllImport] with extern methods
    for older targets via #if conditional compilation.
    
    Closes NordSecurity/uniffi-bindgen-cs#155
    
    Signed-off-by: Mathews Bryan <37884221+jmbryan4@users.noreply.github.com>

--- bindgen/templates/NamespaceLibraryTemplate.cs
+++ bindgen/templates/NamespaceLibraryTemplate.cs
@@ -3,7 +3,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */#}
 
 // This is an implementation detail that will be called internally by the public API.
+#if NET8_0_OR_GREATER
+static partial class _UniFFILib {
+#else
 static class _UniFFILib {
+#endif
     {%- for def in ci.ffi_definitions() %}
     {%- match def %}
     {%- when FfiDefinition::CallbackFunction(callback) %}
@@ -34,8 +38,15 @@ static class _UniFFILib {
     }
 
     {% for func in ci.iter_ffi_function_definitions() -%}
+#if NET8_0_OR_GREATER
+    [LibraryImport("{{ config.cdylib_name() }}")]
+    [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static partial
+#else
     [DllImport("{{ config.cdylib_name() }}", CallingConvention = CallingConvention.Cdecl)]
-    public static extern {%- match func.return_type() -%}{%- when Some with (type_) %} {{ type_.borrow()|ffi_type_name }}{% when None %} void{% endmatch %} {{ func.name() }}(
+    public static extern
+#endif
+    {% match func.return_type() -%}{%- when Some with (type_) %} {{ type_.borrow()|ffi_type_name }}{% when None %} void{% endmatch %} {{ func.name() }}(
         {%- call cs::arg_list_ffi_decl(func) %}
     );
 
